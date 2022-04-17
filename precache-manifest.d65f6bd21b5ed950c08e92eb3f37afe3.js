ecfeae3560b791a126d6a5bb72255ca2fad59853
Jaswal1p <Jaswal1p@yahoo.com>
2022-04-17 02:24:21 -0400

    Updates

---
+++ precache-manifest.d65f6bd21b5ed950c08e92eb3f37afe3.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8d6c0e0e20eb88d99ad",
+    "url": "/wonderful-react-portfolio/static/css/main.aa2b8079.chunk.css"
+  },
+  {
+    "revision": "e8d6c0e0e20eb88d99ad",
+    "url": "/wonderful-react-portfolio/static/js/main.e8d6c0e0.chunk.js"
+  },
+  {
+    "revision": "517527aa6758397229f0",
+    "url": "/wonderful-react-portfolio/static/js/1.517527aa.chunk.js"
+  },
+  {
+    "revision": "9966278125963d93adcb",
+    "url": "/wonderful-react-portfolio/static/js/2.99662781.chunk.js"
+  },
+  {
+    "revision": "5b732da54efb4c9f829b",
+    "url": "/wonderful-react-portfolio/static/js/runtime~main.5b732da5.js"
+  },
+  {
+    "revision": "fd4c6132e678e0102069d185d6807cbf",
+    "url": "/wonderful-react-portfolio/static/media/background.fd4c6132.jpg"
+  },
+  {
+    "revision": "ddb1c5e4c0b38544c652e09cdc543ff9",
+    "url": "/wonderful-react-portfolio/static/media/profilePic.ddb1c5e4.jpg"
+  },
+  {
+    "revision": "f8b82c921650e405df2724c8b15cda16",
+    "url": "/wonderful-react-portfolio/static/media/UConn-Logo.f8b82c92.jpg"
+  },
+  {
+    "revision": "52f1b7358c35b678deabc29e3bcf59eb",
+    "url": "/wonderful-react-portfolio/static/media/UConn-Health.52f1b735.png"
+  },
+  {
+    "revision": "f064362b410b80b3d65bb00606cc3524",
+    "url": "/wonderful-react-portfolio/static/media/Yale-Logo.f064362b.png"
+  },
+  {
+    "revision": "ec426761916607312fbe865a71f3cf3a",
+    "url": "/wonderful-react-portfolio/static/media/MSU-Logo.ec426761.png"
+  },
+  {
+    "revision": "e13662a7289248da62cc3a9a1e653131",
+    "url": "/wonderful-react-portfolio/static/media/PhotoPort.e13662a7.png"
+  },
+  {
+    "revision": "89febc6aee4b47a98877d5b3f1c50680",
+    "url": "/wonderful-react-portfolio/static/media/myHealthChart.89febc6a.png"
+  },
+  {
+    "revision": "bfdcb4c61bc1adeb67d7b5f612a84bd4",
+    "url": "/wonderful-react-portfolio/static/media/PeachTechBlog.bfdcb4c6.png"
+  },
+  {
+    "revision": "0bb7937edde791a9abd48ee81fe74e71",
+    "url": "/wonderful-react-portfolio/static/media/PwaBudgetTracker.0bb7937e.png"
+  },
+  {
+    "revision": "ca24a14fa1b719ee275cbba96bd8da24",
+    "url": "/wonderful-react-portfolio/static/media/RecepieBudget.ca24a14f.png"
+  },
+  {
+    "revision": "60c22a90471cf8a4a5de402364cd3b53",
+    "url": "/wonderful-react-portfolio/static/media/GitItDone.60c22a90.png"
+  },
+  {
+    "revision": "072e8031ba3a23aeda079f0b3ea83b3a",
+    "url": "/wonderful-react-portfolio/static/media/jaswal_resume2022.072e8031.png"
+  },
+  {
+    "revision": "0fb94049a46d049db302592eb0c2ba15",
+    "url": "/wonderful-react-portfolio/index.html"
+  }
+];
